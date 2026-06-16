23ce79fa8de9425bd0ca5836d71b8548c9e23dfc
yorda1981 <yordasr@gmail.com>
2026-06-16 14:57:33 -0300

    Delete flowai-server directory

--- flowai-server/src/supabase.js
+++
@@ -1,6 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SECRET_KEY
-);
-module.exports = supabase;
