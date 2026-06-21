51996a22b57d7b3e8ad2eb3ff30be431f67d5e15
yorda1981 <yordasr@gmail.com>
2026-06-20 22:39:05 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors({
   origin: '*',
   methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization']
+  allowedHeaders: ['Content-Type','Authorization','x-admin-key']
 }));
 app.use(express.json());
 
